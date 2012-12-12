03ae5dd5f7b124acd757643b2f8f7eff38bfccc1
bpolletvillard <benjamin.polletvillard@gmail.com>
2012-12-12 14:55:17 +0100

    Ajout classes

---
+++ Ascenseur/src/ascenseur/Calendrier.java
@@ -0,0 +1,17 @@
+import java.util.Date;
+
+
+public class Calendrier {
+
+	boolean isWeek;
+	Date date;
+
+	public void determinerPlageHoraire(){
+		
+	}
+	public static void main(String[] args) {
+		
+			
+	}
+
+}
