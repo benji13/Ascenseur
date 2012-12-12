c35d31697af0d4d8bd48d017fb5f7d9f41e4822f
Thibaut <Thibaut@134.214.51.216>
2012-12-12 15:04:53 +0100

    Debut de classe ascenseur

---
+++ Ascenseur/src/ascenseur/Ascenseur.java
@@ -0,0 +1,38 @@
+package ascenseur;
+import java.util.ArrayList;
+
+public class Ascenseur {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// Variables
+		int idAscenseur;
+		int etatAscenseur;
+		ArrayList<Appel> tabAppelAtraiter;
+		ArrayList<Appel> tabAppelsTraités;
+		ArrayList<Integer> tabDestination;
+		int positionActuelle;
+		int nbPersonne;
+		int consommation;
+		int directionEnCours;
+
+	}
+
+	private static void triAppel() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	private static void deplacement() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	private static void traitementAppel() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
--- Ascenseur/src/ascenseur/ascenseur.java
+++
@@ -1,5 +0,0 @@
-package ascenseur;
-
-public class ascenseur {
-
-}
