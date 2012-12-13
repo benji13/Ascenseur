6af66fe5beac4f7ad38a284507dba45277b21ef4
Lagueudo <mohamedfadyl.ndiaye@gmail.com>
2012-12-13 16:27:48 +0100

    [Statistique] CalculTotalDuree: implementation

--- Ascenseur/src/ascenseur/Statistiques.java
+++ Ascenseur/src/ascenseur/Statistiques.java
@@ -1,24 +1,33 @@
 package ascenseur;
 
 import java.util.ArrayList;
-import java.util.Date;
+import java.util.Calendar;
 
 public class Statistiques {
- Integer tabnbAppel [];
- Integer tabConsoMoyenne [];
- Integer consoTotal;
- Integer nbAppelTotal;
- 
- 
- int calculTotalDuree(ArrayList <Appel> tabAppel){
 	
- }
- 
- Date calculDureeSimu(){
+	 ArrayList <Integer> tabnbAppel;
+	 ArrayList <Integer> tabConsoMoyenne;
+	 ArrayList <Integer> consoTotal;
+	 ArrayList <Integer> nbAppelTotal;
 	 
- }
- 
- void calculTotalConso(ArrayList <Ascenseur> tabAscenseur){
 	 
- }
+	 int calculTotalDuree(ArrayList <Appel> tabAppel){
+		int total = 0;
+		int i,duree;
+		
+		for(i=0;i<tabAppel.size();i++)
+		{
+			duree = (int) (tabAppel.get(i).getDateFin().getDate().getTimeInMillis() - tabAppel.get(i).getDateDebut().getDate().getTimeInMillis());
+			total+=duree;
+		}
+		 return total;
+	 }
+	 
+	 Calendrier calculDureeSimu(){
+		 
+	 }
+	 
+	 void calculTotalConso(ArrayList <Ascenseur> tabAscenseur){
+		 
+	 }
 }
