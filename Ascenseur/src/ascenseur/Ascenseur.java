55653b6f7ad5adc8119d8e884195ebb10807b586
Thibaut <Thibaut@pc-link>
2012-12-15 21:40:37 +0100

    [Ascenseur] Version Thibaut !

--- Ascenseur/src/ascenseur/Ascenseur.java
+++ Ascenseur/src/ascenseur/Ascenseur.java
@@ -4,13 +4,20 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 public class Ascenseur {
-
+	
+	/**
+	 * @return
+	 */
+	public Ascenseur() {
+		// TODO Auto-generated constructor stub
+	}
+	
 	/**
 	 * @param args
 	 */
 	// VARIABLES
 	private int idAscenseur;
-	private int etatAscenseur;
+	private boolean etatAscenseur; //0 arret; 1 en deplacement
 	private ArrayList<Appel> tabAppelAtraiter;
 	private ArrayList<Appel> tabAppelsTraites;
 	private ArrayList<Integer> tabDestination;
@@ -18,9 +25,23 @@ public class Ascenseur {
 	private int nbPersonne;
 	private int consommation;
 	private int directionEnCours; //0 monte ; 1 descend
+	private int positionRepo;
+	private int tempsParcoursAscenseur;
 	
 	
 	// GETTER ET SETTER
+	public int getTempsParcoursAscenseur() {
+		return tempsParcoursAscenseur;
+	}
+	public void setTempsParcoursAscenseur(int tempsParcoursAscenseur) {
+		this.tempsParcoursAscenseur = tempsParcoursAscenseur;
+	}
+	public void setPositionRepo(int positionRepo) {
+		this.positionRepo = positionRepo;
+	}
+	public int getPositionRepo() {
+		return positionRepo;
+	}
 	public int getDirectionEnCours() {
 		return directionEnCours;
 	}
@@ -36,7 +57,7 @@ public class Ascenseur {
 	public int getConsommation() {
 		return consommation;
 	}
-	public int getEtatAscenseur() {
+	public boolean getEtatAscenseur() {
 		return etatAscenseur;
 	}
 	public int getNbPersonne() {
@@ -51,7 +72,7 @@ public class Ascenseur {
 	public void setConsommation(int consommation) {
 		this.consommation = consommation;
 	}
-	public void setEtatAscenseur(int etatAscenseur) {
+	public void setEtatAscenseur(boolean etatAscenseur) {
 		this.etatAscenseur = etatAscenseur;
 	}
 	public void setNbPersonne(int nbPersonne) {
@@ -77,11 +98,110 @@ public class Ascenseur {
 	}
 	
 	// FONCTIONS
-	private void triAppel() {
+
+	/**
+	 * Fonction permettant de déplacer un ascenseur d'un étage à un autre.
+	 * Deux cas possible : Appel ou Repositionnement
+	 * @return
+	 * @throws InterruptedException
+	 */
+	public void deplacement() throws InterruptedException {
+		// TODO Auto-generated method stub
+		
+		int nbEtageAparcourir;
+		
+		if(tabAppelAtraiter.size() != 0){
+			//ascenseur passe en mouvement
+			etatAscenseur=true;
+			
+			//calcul nombre etage à parcourir
+			nbEtageAparcourir = tabAppelAtraiter.get(0).getDestAppel() - positionActuelle;
+			nbEtageAparcourir = Math.abs(nbEtageAparcourir);
+			
+			//calcul temps du parcours
+			if(nbEtageAparcourir >= 4){
+				Thread.sleep((10+nbEtageAparcourir)*1000);
+			}
+			else if(nbEtageAparcourir == 3){
+				Thread.sleep(8*1000);
+			}
+			else if(nbEtageAparcourir == 2){
+				Thread.sleep(6*1000);
+			}
+			else if(nbEtageAparcourir == 1){
+				Thread.sleep(3*1000);
+			}
+			
+			//changement de l'etage
+			positionActuelle=tabAppelAtraiter.get(0).getDestAppel();
+			
+			//ascenseur passe à l'arret
+			etatAscenseur=false;
+			
+			//les appels correspondant à cet etages passe en traité
+			traitementAppel();	
+		}
+		else{
+			//ascenseur passe en mouvement
+			etatAscenseur=true;
+			
+			//calcul nombre etage à parcourir
+			nbEtageAparcourir = positionRepo - positionActuelle;
+			nbEtageAparcourir = Math.abs(nbEtageAparcourir);
+			
+			
+			//Sleep suivant le nombre d'étage à parcourir
+			sleepParcours(nbEtageAparcourir);
+						
+			//changement de l'etage
+			positionActuelle=positionRepo;
+			
+			//ascenseur passe à l'arret
+			etatAscenseur=false;
+
+		}
+	}
+	
+	/**
+	 * Fonction permettant de rechercher les appels correspondant à l'étage passé en paramètre
+	 * @param etage
+	 * @return
+	 */
+	public ArrayList<Appel> rechercheAppel(Integer etage){
+		ArrayList<Appel> tabAppelTrouve = new ArrayList<Appel>();
+		int i=0;
+		while(i<tabAppelAtraiter.size()){
+			 if(tabAppelAtraiter.get(i).getDestAppel()==etage)
+				 tabAppelTrouve.add(tabAppelAtraiter.get(i));
+			 i++;
+		}
+		return tabAppelTrouve;
+	}
+	
+	/**
+	 * Fonction permettant de dire que nous avons traité un appel
+	 * @return
+	 */
+	public void traitementAppel() {
+		// TODO Auto-generated method stub
+		ArrayList<Appel> tabAppelAsupprimer = new ArrayList<Appel>();
+		
+		// Recherche les appels correspondant à l'etage actuel.
+		tabAppelAsupprimer = rechercheAppel(positionActuelle);
+		
+		//Déplace les appels de Atraiter vers Traiter
+		tabAppelsTraites.addAll(tabAppelAsupprimer);
+		tabAppelAtraiter.removeAll(tabAppelAsupprimer);
+	}
+	
+	/**
+	 * Fonction permettant de trier les appels se trouvant dans AppelsAtraiter afin de les organiser dans l'ordre où l'ascenseur va s'arrêter.
+	 * @return
+	 */
+	public void triAppel() {
 		//Fonction triant les appels en attente
 		
 		//remplissage d'un tableau avec les destinations des appels
-		ArrayList<Integer> TabDest = new ArrayList<Integer>();
 		for (Appel appel : this.tabAppelAtraiter) {
 			tabDestination.add(appel.getDestAppel());
 		}
@@ -95,24 +215,56 @@ public class Ascenseur {
 		}
 	}
 	
-	private void deplacement() {
-		// TODO Auto-generated method stub
+	/**
+	 * Fonction permettant de combien de temps va mettre l'ascenseur pour traiter tout les appels en cours
+	 * @return
+	 */
+	public void calculDureeTraitementAppel(){
+		int i=0;
+		int nbEtageAparcourir;
+		tempsParcoursAscenseur=0;
 		
-	}
-	
-	private void traitementAppel() {
-		// TODO Auto-generated method stub
-		ArrayList<Appel> tabAppelAsupprimer = new ArrayList<Appel>();
-		for (Integer i : this.tabDestination) {
-			if(positionActuelle == i){
-				// A FAIRE : RECHERCHER LES APPELS CORRESPONDANT A CET ETAGE 
-				// LES PLACER DANS tabAppelAsupprimer
-				
+		// pour tout les appels sauf le dernier car pris dans la comparaison
+		for(i=0;i<tabAppelAtraiter.size();i++){
+			
+			//Calcul le nombre d'etage a parcourir entre deux appels consécutifs
+			nbEtageAparcourir = tabAppelAtraiter.get(i+1).getDestAppel()-tabAppelAtraiter.get(i).getDestAppel();
+			nbEtageAparcourir = Math.abs(nbEtageAparcourir);
+			
+			// Calcul du temps à attendre entre le parcours de ces deux etages cumulé avec l'ancien
+			if(nbEtageAparcourir >= 4){
+				tempsParcoursAscenseur = tempsParcoursAscenseur + (10+nbEtageAparcourir)*1000*XTEMPS;
+			}
+			else if(nbEtageAparcourir == 3){
+				tempsParcoursAscenseur = tempsParcoursAscenseur + (8*1000*XTEMPS);
+			}
+			else if(nbEtageAparcourir == 2){
+				tempsParcoursAscenseur = tempsParcoursAscenseur + (6*1000*XTEMPS);
+			}
+			else if(nbEtageAparcourir == 1){
+				tempsParcoursAscenseur = tempsParcoursAscenseur + (3*1000*XTEMPS);
 			}
 		}
-		//Déplace les appels de Atraiter vers Traiter
-		tabAppelsTraites.addAll(tabAppelAsupprimer);
-		tabAppelAtraiter.removeAll(tabAppelAsupprimer);
 	}
-
+	
+	/**
+	 * Fonctione permettant de gerer le temps de parcours entre les etages via des sleep
+	 * @param nbEtageAparcourir
+	 * @throws InterruptedException
+	 */
+	public void sleepParcours(int nbEtageAparcourir) throws InterruptedException{
+		int nbEtage = nbEtageAparcourir;
+		if(nbEtage >= 4){
+			Thread.sleep((10+nbEtage)*1000*XTEMPS);
+		}
+		else if(nbEtage == 3){
+			Thread.sleep(8*1000*XTEMPS);
+		}
+		else if(nbEtage == 2){
+			Thread.sleep(6*1000*XTEMPS);
+		}
+		else if(nbEtage == 1){
+			Thread.sleep(3*1000*XTEMPS);
+		}
+	}
 }
