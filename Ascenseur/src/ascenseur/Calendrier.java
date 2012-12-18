6e2ac83fddf752b7312bad54a022d9240c7acc0b
Thibaut <Thibaut@pc-link.lan>
2012-12-18 23:36:12 +0100

    [Calendrier] calculDateActuelle: Création de la fonction

--- Ascenseur/src/ascenseur/Calendrier.java
+++ Ascenseur/src/ascenseur/Calendrier.java
@@ -4,23 +4,19 @@ import java.util.Calendar;
 
 public class Calendrier {
 
+	// VARIABLES
 	private Chronometre chrono;
 	private boolean isWeek;
-	private Calendar date;
+	private Calendar dateActuelle;
+	private Calendar dateDebutSimu;
+	private int xtemps;
 	
-	
+	// GETTER SETTER
 	public Chronometre getChrono() {
 		return chrono;
 	}
 	public void setChrono(Chronometre chrono) {
 		this.chrono = chrono;
-	}
-	public Calendar getDate() {
-		return date;
-	}
-
-	public void setDate(Calendar date) {
-		this.date = date;
 	}
 	
 	public boolean isWeek() {
@@ -31,9 +27,37 @@ public class Calendrier {
 		this.isWeek = isWeek;
 	}
 	
+	public Calendar getDateActuelle() {
+		return dateActuelle;
+	}
+	
+	public Calendar getDateDebutSimu() {
+		return dateDebutSimu;
+	}
+	
+	public int getXtemps() {
+		return xtemps;
+	}
+	
+	public void setDateActuelle(Calendar dateActuelle) {
+		this.dateActuelle = dateActuelle;
+	}
+	
+	public void setDateDebutSimu(Calendar dateDebutSimu) {
+		this.dateDebutSimu = dateDebutSimu;
+	}
+	
+	public void setXtemps(int xtemps) {
+		this.xtemps = xtemps;
+	}
+	//FONCTIONS
 	public void determinerPlageHoraire(){
-
 		
+	}
+	
+	public void calculDateActuelle(){
+		// A FINIR. Ajouter des sec à la date de début de simu
+		this.dateActuelle = this.dateDebutSimu.add(Calendar.SECOND, (chrono.getTempsEcouleSecs()*xtemps));
 	}
 
 }
