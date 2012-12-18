7b6fbe98b16fce51277d5446af05bc64932a483e
Lagueudo <mohamedfadyl.ndiaye@gmail.com>
2012-12-18 18:24:29 +0100

    [ASCENSEUR]getteur et setteur

--- Ascenseur/src/ascenseur/Statistiques.java
+++ Ascenseur/src/ascenseur/Statistiques.java
@@ -12,6 +12,40 @@ public class Statistiques {
 	 Integer nbAppelTotal;
 	 Date totalDuree;
 	 
+	 public Integer getConsoTotal() {
+		return consoTotal;
+	}
+	 public Integer getNbAppelTotal() {
+		return nbAppelTotal;
+	}
+	public ArrayList<Integer> getTabConso() {
+		return tabConso;
+	}
+	public ArrayList<Integer> getTabnbAppel() {
+		return tabnbAppel;
+	}
+	public Date getTotalDuree() {
+		return totalDuree;
+	}
+	public void setConsoTotal(Integer consoTotal) {
+		this.consoTotal = consoTotal;
+	}
+	public void setNbAppelTotal(Integer nbAppelTotal) {
+		this.nbAppelTotal = nbAppelTotal;
+	}
+	public void setTabConso(ArrayList<Integer> tabConso) {
+		this.tabConso = tabConso;
+	}
+	public void setTabnbAppel(ArrayList<Integer> tabnbAppel) {
+		this.tabnbAppel = tabnbAppel;
+	}
+	public void setTotalDuree(Date totalDuree) {
+		this.totalDuree = totalDuree;
+	}
+	/**
+	 *  
+	 * @param unBatterie
+	 */
 	 void calculTotalDuree(Batterie unBatterie){
 	
 	 }
@@ -24,7 +58,10 @@ public class Statistiques {
 			this.consoTotal += tabAscenseur.get(i).getConsommation();
 		}
 	}
-
+/**
+ * 
+ * @param tabAscenseur
+ */
 	 void calculNbTotalAppel(ArrayList <Ascenseur> tabAscenseur){
 		int i;
 			
@@ -33,7 +70,10 @@ public class Statistiques {
 			this.nbAppelTotal += tabAscenseur.get(i).getTabAppelsTraites().size();
 		}
 	 }
-
+/**
+ * 
+ * @param tabAscenseur
+ */
 	 //Calcule le nombre d'appel par ascenseur, il met à jour le tableau tabnbAppel
 	 void calculNbAppel(ArrayList <Ascenseur> tabAscenseur){
 		 int i;
@@ -43,7 +83,10 @@ public class Statistiques {
 			 this.tabnbAppel.set(i, tabAscenseur.get(i).getTabAppelsTraites().size());
 		 }
 	 }
-	 
+/**
+ * 	 
+ * @param tabAscenseur
+ */
 	//Calcule la conso totale par ascenseur, il met à jour le tableau tabConso
 	 void calculConso(ArrayList <Ascenseur> tabAscenseur){
 		 int i;
