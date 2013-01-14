91ccf89f0c47546e2fbdab7a6a993cc6429c8ac7
Lagueudo <mohamedfadyl.ndiaye@gmail.com>
2013-01-14 17:26:05 +0100

    [Seconde]

---
+++ Ascenseur/src/ascenseur/Seconde.java
@@ -0,0 +1,13 @@
+package ascenseur;
+
+public class Seconde {
+	 public synchronized void attenteSeconde   (int nbSec) throws InterruptedException {
+		 
+		 	for(int i=0;i<nbSec;i++){
+		 		//System.out.println("Attente "+i+" seconde");
+		 		wait () ; 
+		 	}
+		 }
+	 public synchronized void declenchementSeconde () { /*System.out.println("Declenchement seconde");*/notifyAll () ; }
+
+}
